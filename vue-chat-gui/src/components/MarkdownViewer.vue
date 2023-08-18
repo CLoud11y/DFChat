b7701ac3643d561a8fdab4a29279349cfebbeb15
SaintJR <1911619172@qq.com>
2023-08-18 15:18:25 +0800

    光标

--- vue-chat-gui/src/components/MarkdownViewer.vue
+++ vue-chat-gui/src/components/MarkdownViewer.vue
@@ -1,11 +1,12 @@
 <template>
+    <Transition name=“blink” v-show="show&&!markdownContent" > <span>|</span> </Transition>
   <div class="markdown-body" v-html="markdownContent"/>
 </template>
 <script>
 import MarkdownIt from 'markdown-it';
 import markdownItCodeCopy from 'markdown-it-code-copy';
 import 'github-markdown-css/github-markdown.css';
-
+import { watch } from 'vue';
 export default {
   props: {
       markdown: {
@@ -15,9 +16,17 @@ export default {
     },
   data() {
     return {
-      md: null
+      md: null,
+      show : false,
+      timer:null
     };
   },
+  // watch:{
+  //  markdownContent:function(){
+  //   this.show = false
+  //   console.log('@@@@@@@@@值改变了 准备取消定时器')
+  //  }
+  // },
   computed: {
     markdownContent() {
       return this.md.render(this.markdown);
@@ -28,21 +37,30 @@ export default {
       .use(markdownItCodeCopy, {
         // Options
       });
-  }
+  },
+  mounted(){
+   const timer = setInterval(() => { this.show = !this.show; }, 500);
+  },
+
+
 };
 </script>
 <style>
+.blink-enter-active, .blink-leave-active { animation: blink 0.5s; }
+
+@keyframes blink { from { opacity: 1; } to { opacity: 0; } }
 .markdown-it-code-copy {
   cursor: pointer;
   position: absolute;
   right: 0;
+
 }
 .markdown-it-code-copy::before {
   content: "copy";
   display: inline-block;
   padding: 0 1em;
   background-color: #f0f0f0;
-  color: #999;
+
 }
 .markdown-it-code-copy:hover::before {
   background-color: #007bff;
@@ -54,10 +72,9 @@ export default {
 .markdown-body{
   padding: 0;
 }
-/* .user .markdown-body  {
+ .user .markdown-body  {
   background-color: inherit;
-} */
-.markdown-body  {
+}  .markdown-body  {
   background-color: inherit;
-}
+} 
 </style>
